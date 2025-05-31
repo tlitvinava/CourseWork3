853cb5244b26366fddaf256efd5172246534657b
tlitvinava <t.litvinava@gmail.com>
2025-06-01 01:53:51 +0300

    change main  models

--- modules/models/user.py
+++ modules/models/user.py
@@ -11,28 +11,3 @@ class User:
         self.region = region
         self.favorites = favorites if favorites is not None else []
         self.friends = friends if friends is not None else []
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "password": self.password,
-            "phone": self.phone,
-            "email": self.email,
-            "region": self.region,
-            "favorites": self.favorites,
-            "friends": self.friends,
-        }
-
-    @classmethod
-    def from_dict(cls, d):
-        return cls(
-            username=d.get("username"),
-            password=d.get("password"),
-            phone=d.get("phone"),
-            email=d.get("email"),
-            region=d.get("region"),
-            favorites=d.get("favorites", []),
-            id=d.get("id"),
-            friends=d.get("friends", []),
-        )

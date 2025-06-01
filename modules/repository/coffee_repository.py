cb88641c8a113e7450d903f3055087118eb93aed
tlitvinava <t.litvinava@gmail.com>
2025-06-01 12:05:38 +0300

    rewrite servises algo

--- modules/repository/coffee_repository.py
+++ modules/repository/coffee_repository.py
@@ -1,31 +1,34 @@
-# -*- coding: utf-8 -*-
+from modules.models.coffeeshop import CoffeeShop
+from modules.repository.mongo_repository import MongoRepository
+from TSerializerPack.serializer import UniversalSerializer
 
-class CoffeeShopRepository:
-    def __init__(self):
-        self.coffee_shops = {}
+class CoffeeShopRepository(MongoRepository):
+    def __init__(self, uri="mongodb://localhost:27017", db_name="my_database"):
+        super().__init__(uri, db_name)
+        self.collection = self.db["coffeehouses"]
+        self.serializer = UniversalSerializer(CoffeeShop)
 
-    def add(self, coffee_shop):
-        self.coffee_shops[coffee_shop.id] = coffee_shop
-        return coffee_shop
+    def add_coffee_shop(self, coffe_shop):
+        data = self.serializer._to_dict(coffe_shop)
+        self.collection.insert_one(data)
+        return coffe_shop
 
-    def list_all(self):
-        return list(self.coffee_shops.values())
+    def update_coffee_shop(self, coffe_shop):
+        data = self.serializer._to_dict(coffe_shop)
+        result = self.collection.replace_one({"id": coffe_shop.get('id')}, data, upsert=True)
+        print(f"Modified count: {result.modified_count}")
+        return coffe_shop
 
-    def get(self, shop_id):
-        return self.coffee_shops.get(shop_id)
-
-    def update(self, shop_id, update_data):
-        shop = self.get(shop_id)
-        if shop:
-            for key, value in update_data.items():
-                if key == "name":
-                    shop.name = value
-                elif key == "address":
-                    shop.address = value
-                elif key == "attributes":
-                    shop.attributes = value
-            return shop
+    def get_coffe_shop_by_id(self, coffe_shop_id):
+        data = self.collection.find_one({"id": coffe_shop_id})
+        if data:
+            return self.serializer._from_dict(data)
         return None
 
-    def delete(self, shop_id):
-        return self.coffee_shops.pop(shop_id, None)
+    def find_many(self, filter_query=None, skip=0, limit=10):
+        filter_query = filter_query or {}
+        cursor = self.collection.find(filter_query).skip(skip).limit(limit)
+        items = [self.serializer._from_dict({k: v for k, v in doc.items() if k != "_id"}) for doc in cursor]
+        total = self.collection.count_documents(filter_query)
+        return {"results": items, "total": total}
+
--- modules/services/coffee_shop_service.py
+++ modules/services/coffee_shop_service.py
@@ -2,16 +2,33 @@
 from modules.models.coffeeshop import CoffeeShop
 from modules.repository.coffee_repository import CoffeeShopRepository
 
+
 class CoffeeShopService:
     def __init__(self, repository=None):
         if repository is None:
             repository = CoffeeShopRepository()
         self.repository = repository
 
-    def create_coffee_shop(self, name, address, **attributes):
-        shop = CoffeeShop(name, address, **attributes)
-        return self.repository.add(shop)
+    def get_shops(self, page=1, per_page=10, query=None):
+        offset = (page - 1) * per_page
+        filter_query = {}
+        if query:
+            filter_query["data.name"] = {"$regex": query, "$options": "i"}
+        return self.repository.find_many(filter_query, skip=offset, limit=per_page)
 
-    def list_coffee_shops(self):
-        shops = self.repository.list_all()
-        return [shop.to_dict() for shop in shops]
+    def add_or_update_coffee_shop(self, coffee_shop):
+        existing = self.repository.get_coffe_shop_by_id(coffee_shop.get('id'))
+        if existing is None:
+            return self.repository.add_coffee_shop(coffee_shop)
+        else:
+            return self.repository.update_coffee_shop(coffee_shop)
+
+    def count_coffee_shops(self, query=None):
+        filter_query = {}
+        if query:
+            filter_query["tags.name"] = {"$regex": query, "$options": "i"}
+        return self.repository.find_many(filter_query).get("total", 0)
+
+    def get_coffee_shop_by_id(self, coffe_shop_id):
+        return self.repository.get_coffe_shop_by_id(coffe_shop_id)
+
